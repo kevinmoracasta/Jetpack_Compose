e9be62b3d8d2f25d218ac39b1c3b8537f140f2f8
kevinmoracasta <126214607+kevinmoracasta@users.noreply.github.com>
2023-08-09 09:56:32 -0500

    Update SampleData.kt

--- app/src/main/java/co/edu/sena/myapp2687386/SampleData.kt
+++ app/src/main/java/co/edu/sena/myapp2687386/SampleData.kt
@@ -4,11 +4,11 @@ object SampleData {
     // Sample conversation data
     val conversationSample = listOf(
         Message(
-            "Colleague",
+            "Kevin",
             "Test...Test...Test..."
         ),
         Message(
-            "Colleague",
+            "Kevin",
             "List of Android versions:\n" +
                     "Android KitKat (API 19)\n" +
                     "Android Lollipop (API 21)\n" +
@@ -21,39 +21,39 @@ object SampleData {
                     "Android 12 (API 31)\n"
         ),
         Message(
-            "Colleague",
+            "Kevin",
             "I think Kotlin is my favorite programming language.\n" +
                     "It's so much fun!"
         ),
         Message(
-            "Colleague",
+            "Kevin",
             "Searching for alternatives to XML layouts..."
         ),
         Message(
-            "Colleague",
+            "Kevin",
             "Hey, take a look at Jetpack Compose, it's great!\n" +
                     "It's the Android's modern toolkit for building native UI." +
                     "It simplifies and accelerates UI development on Android." +
                     "Less code, powerful tools, and intuitive Kotlin APIs :)"
         ),
         Message(
-            "Colleague",
+            "Kevin",
             "It's available from API 21+ :)"
         ),
         Message(
-            "Colleague",
+            "Kevin",
             "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
         ),
         Message(
-            "Colleague",
+            "Kevin",
             "Android Studio next version's name is Arctic Fox"
         ),
         Message(
-            "Colleague",
+            "Kevin",
             "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
         ),
         Message(
-            "Colleague",
+            "Kevin",
             "I didn't know you can now run the emulator directly from Android Studio"
         ),
         Message(
@@ -61,11 +61,11 @@ object SampleData {
             "Compose Previews are great to check quickly how a composable layout looks like"
         ),
         Message(
-            "Colleague",
+            "Kevin",
             "Previews are also interactive after enabling the experimental setting"
         ),
         Message(
-            "Colleague",
+            "Kevin",
             "Have you tried writing build.gradle with KTS?"
         ),
     )
